4651dcbb695c03cd2590f7d600f4deb9a0495ed4
vandann <dansolt@gmail.com>
2020-04-05 19:42:48 -0400

    final edit for the day
    played with exercies in 10  and cleaned up random docs

--- mystuff/ex10.py
+++ mystuff/ex10.py
@@ -5,14 +5,14 @@ persian_cat = "I'm split\non a line."
 # sets var backslash_cat = string with escape(backslashes)
 backslash_cat = "I'm \\ a \\ cat."
 
+cats_at_home = "\n {} \n{} \n\n\n\n\n \t\t\t\t\t\t\t\t {}"
 # sets var fat_cat = block (""") string with individual rows & bullets
-fat_cat = """
+fat_cat = '''
 I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
-"""
-
+'''
 # outputs tabby_cat with escape formatting
 print(tabby_cat)
 # outputs persian_cat with escape formatting
@@ -21,3 +21,5 @@ print(persian_cat)
 print(backslash_cat)
 # outputs fat_cat with """ and escape formatting
 print(fat_cat)
+
+print(cats_at_home.format(tabby_cat, persian_cat, backslash_cat))
