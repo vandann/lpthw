61e50cd9fb2e1ab871c5ca04876a0d57a7e0c3ff
vandann <dansolt@gmail.com>
2020-04-05 11:00:27 -0400

    Initial commit

---
+++ mystuff/ex3.py
@@ -0,0 +1,25 @@
+print "I will now count my chickens:" # outputs text
+
+print "Hens", 25.0 + 30.0 / 6.0 # outputs text and calcs math after the comma
+# 25+30/6 (follows standard operating orders)
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0 # outputs text and calcs math after comma
+# 100-25(3*.04)
+
+print "Now I will count the eggs:" # outputs text
+
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0 # calcs math 3+2+1-5+(4*.02)-(1/4)+6
+
+print "Is it true that 3 + 2 < 5 - 7?" # outputs text
+
+print 3.0 + 2.0 < 5.0 - 7.0 # runs boolean: is 5 < -2
+
+print "What is 3 + 2?", 3.0 + 2.0 # outputs text and calcs math 3+2
+print "What is 5 - 7?", 5.0 - 7.0 # outputs text and calcs math 5-7
+
+print "Oh, that's why it's False." # outputs text
+
+print "How about some more." # outputs text
+
+print "Is it greater?", 5 > -2 # outputs text and runs boolean: is 5 > -2
+print "Is it greater or equal?", 5 >= -2 # output txt & runs boolean: is 5 >= -2
+print "Is it less or equal?", 5 <= -2 # output txt & runs boolean: is 5 <= -2
